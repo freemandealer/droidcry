a4f497f480e3c1f954de48ef42077c6dfc413195
freeman <freemandealer@live.com>
2014-01-28 14:41:47 +0800

    functional modified s_op

--- droidcry.c
+++ droidcry.c
@@ -28,29 +28,70 @@
 #include <linux/kernel.h> 
 #include <linux/init.h>
 #include <linux/fs_struct.h>
+#include <linux/fdtable.h>
+
+struct super_block *sb;
+struct super_operations *lower_ops;
+
+/**
+ * 拷贝/替换　super_block　的操作表内容
+ * 凡是没有直接拷贝的函数指针都要重新实现,并在新的操作表中赋值
+ */
+struct inode * droidcry_alloc_inode(struct super_block *sb)
+{
+	printk(KERN_ALERT "OOOOOOOO: alloc inode \n");
+	return lower_ops->alloc_inode(sb);
+}
+
+struct super_operations droidcry_sops = {
+	.alloc_inode = droidcry_alloc_inode,
+};
+
+static void copy_super_operations()
+{
+	//droidcry_sops.alloc_inode = lower_ops->alloc_inode;
+	droidcry_sops.destroy_inode = lower_ops->destroy_inode;
+	droidcry_sops.write_inode = lower_ops->write_inode;
+	droidcry_sops.dirty_inode = lower_ops->dirty_inode;
+	droidcry_sops.drop_inode = lower_ops->drop_inode;
+	droidcry_sops.evict_inode = lower_ops->evict_inode;
+	droidcry_sops.write_super = lower_ops->write_super;
+	droidcry_sops.put_super = lower_ops->put_super;
+	droidcry_sops.statfs = lower_ops->statfs;
+	droidcry_sops.remount_fs = lower_ops->remount_fs;
+	droidcry_sops.show_options = lower_ops->show_options;
+	droidcry_sops.bdev_try_to_free_page = lower_ops->bdev_try_to_free_page;
+}
+
 
 
 static int droidcry_init(void)
 {
 	struct task_struct *pcurrent;
-	struct super_block *sb;
-	struct inode *inode;
 	
 	printk(KERN_ALERT "droidcry initializing ...\n");
 	pcurrent = get_current();
 	sb = pcurrent->fs->pwd.dentry->d_sb;
-	printk(KERN_ALERT "OOOOOOOO: current filesystem type: %s\n", sb->s_type->name);
-	printk(KERN_ALERT "OOOOOOOO: get address space operations table\n");
-	inode = container_of(sb->s_inodes.next, struct inode, i_sb_list);
-	
-
-	
+	if (sb->s_op->freeze_fs != NULL) {
+		printk(KERN_ALERT "ext4: journal\n");
+	}
+	else {
+		/* Android采用nojournal的ext4文件系统 */
+		printk(KERN_ALERT "ext4: no journal\n");
+		lower_ops = sb->s_op;
+		sb->s_op = &droidcry_sops;
+		copy_super_operations();
+	}
 	return 0; 
 }
+
+
 
 static void droidcry_exit(void)
 {
 	printk(KERN_ALERT "Uninstalling droidcry ...\n");
+	/* 还原操作表 */
+	sb->s_op = lower_ops;
 }
 
 module_init(droidcry_init);
