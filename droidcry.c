6477e824757cee4ccb03ea58d3e18ee1d61294e5
freeman <freemandealer@live.com>
2014-02-18 12:43:47 +0800

    fuctional framework

--- droidcry.c
+++ droidcry.c
@@ -4,7 +4,6 @@
 #include <linux/module.h>
 #include <linux/namei.h>
 #include <linux/skbuff.h>
-#include <linux/crypto.h>
 #include <linux/mount.h>
 #include <linux/pagemap.h>
 #include <linux/key.h>
@@ -17,7 +16,6 @@
 #include <linux/fs.h>
 #include <linux/scatterlist.h>
 #include <linux/hash.h>
-#include <linux/nsproxy.h>
 #include <linux/sched.h>
 #include <linux/kernel.h> 
 #include <linux/init.h>
@@ -25,64 +23,52 @@
 #include <linux/fdtable.h>
 
 
-struct inode_operations *lower_iops;
-struct address_space_operations *lower_aops;
-struct file_operations *lower_fops;
+const struct inode_operations *lower_iops;
+const struct address_space_operations *lower_aops;
+const struct file_operations *lower_fops; 
 
 
 /**
- * 拷贝/替换　address_space　的操作表内容
- * 凡是没有直接拷贝的函数指针都要重新实现,并在新的操作表中赋值
+ * Copy or replace *address_space* operations
+ * All non-copy operations must be implemented and registered.
  */
 static int droidcry_readpage(struct file *file, struct page *page)
 {
-	printk(KERN_ALERT "OOOOOOOO: my own readpage\n");
+	printk(KERN_ALERT "droidcry: my own readpage\n");
 	return lower_aops->readpage(file, page);
 }
 
-static int droidcry_readpages(struct file *file, struct address_space *mapping, struct list_head *pages, unsigned nr_pages)
+static int droidcry_readpages(struct file *file, struct address_space *mapping, 
+					struct list_head *pages, unsigned nr_pages)
 {
-	printk(KERN_ALERT "OOOOOOOO: my own readpages\n");
+	printk(KERN_ALERT "droidcry: my own readpages\n");
 	return lower_aops->readpages(file, mapping, pages, nr_pages);	
 };
 
 
 static int droidcry_writepage(struct page *page, struct writeback_control *wbc)
 {
-	printk(KERN_ALERT "OOOOOOOO: my own writepage\n");
+	printk(KERN_ALERT "droidcry: my own writepage\n");
 	return lower_aops->writepage(page, wbc);
 }
 
-static int droidcry_writepages(struct address_space *mapping, struct writeback_control *wbc)
+static int droidcry_writepages(struct address_space *mapping, 
+						struct writeback_control *wbc)
 {
-	printk(KERN_ALERT "OOOOOOOO: my own writepages\n");
+	printk(KERN_ALERT "droidcry: my own writepages\n");
 	return lower_aops->writepages(mapping, wbc);
 }
 
 
-static ssize_t droidcry_direct_IO(int rw, struct kiocb *iocb, const struct iovec *iov, loff_t offset, unsigned long nr_segs) 
+static ssize_t droidcry_direct_IO(int rw, struct kiocb *iocb,
+					const struct iovec *iov, loff_t offset, unsigned long nr_segs) 
 {
-	printk(KERN_ALERT "OOOOOOOO: my own direct_IO\n");
+	printk(KERN_ALERT "droidcry: my own direct_IO\n");
 	return lower_aops->direct_IO(rw, iocb, iov, offset, nr_segs);
 }
 
 
-static int droidcry_write_begin(struct file *file, struct address_space *mapping,
-			    loff_t pos, unsigned len, unsigned flags,
-			    struct page **pagep, void **fsdata)
-{
-	printk(KERN_ALERT "OOOOOOOO: my own write_begin\n");
-	return lower_aops->write_begin(file, mapping, pos, len, flags, pagep, fsdata);
-}
 
-static int droidcry_write_end(struct file *file,
-				     struct address_space *mapping,
-				     loff_t pos, unsigned len, unsigned copied,
-				     struct page *page, void *fsdata)
-{
-	printk(KERN_ALERT "OOOOOOOO: my own write_end\n");
-	return lower_aops->write_end(file, mapping, pos, len, copied, page, fsdata);
-}
 
 struct address_space_operations droidcry_aops = {
 	.readpage = droidcry_readpage,
@@ -90,40 +76,41 @@ struct address_space_operations droidcry_aops = {
 	.writepage = droidcry_writepage,
 	.writepages = droidcry_writepages,
 	.direct_IO = droidcry_direct_IO,
-	.write_begin = droidcry_write_begin,
-	.write_end = droidcry_write_end,
 };
 
-
-void droidcry_copy_address_space_operations(struct address_space_operations dest, struct address_space_operations *src)
+/**
+ * make sure lower_iops is correctly pointed before invoke
+ */
+void droidcry_copy_address_space_operations(void)
 {
-	/* 使用前确定lower_aops准备就绪 */
-	//dest.writepage		= src->writepage;
-	//dest.readpage		= src->readpage;
-	//dest.writepages	= src->writepages;
-	dest.set_page_dirty= src->set_page_dirty;
-	//dest.readpages		= src->readpages;
-	//dest.write_begin	= src->write_begin;
-	//dest.write_end		= src->write_end;
-	dest.bmap			= src->bmap;
-	dest.invalidatepage= src->invalidatepage;
-	dest.releasepage	= src->releasepage;
-	dest.freepage		= src->freepage;
-	//dest.direct_IO		= src->direct_IO;
-	dest.get_xip_mem	= src->get_xip_mem;
-	dest.migratepage	= src->migratepage;
-	dest.launder_page	= src->launder_page;
-	dest.is_partially_uptodate= src->is_partially_uptodate;
-	dest.error_remove_page	= src->error_remove_page;
+	//droidcry_aops.writepage		= lower_aops->writepage;
+	//droidcry_aops.readpage		= lower_aops->readpage;
+	//droidcry_aops.writepages	= lower_aops->writepages;
+	droidcry_aops.set_page_dirty= lower_aops->set_page_dirty;
+	//droidcry_aops.readpages		= lower_aops->readpages;
+	droidcry_aops.write_begin	= lower_aops->write_begin;
+	droidcry_aops.write_end		= lower_aops->write_end;
+	droidcry_aops.bmap			= lower_aops->bmap;
+	droidcry_aops.invalidatepage= lower_aops->invalidatepage;
+	droidcry_aops.releasepage	= lower_aops->releasepage;
+	droidcry_aops.freepage		= lower_aops->freepage;
+	//droidcry_aops.direct_IO		= lower_aops->direct_IO;
+	droidcry_aops.get_xip_mem	= lower_aops->get_xip_mem;
+	droidcry_aops.migratepage	= lower_aops->migratepage;
+	droidcry_aops.launder_page	= lower_aops->launder_page;
+	droidcry_aops.is_partially_uptodate= lower_aops->is_partially_uptodate;
+	droidcry_aops.error_remove_page	= lower_aops->error_remove_page;
 }
 
+
+
 /**
- * 拷贝/替换　file　的操作表内容
- * 凡是没有直接拷贝的函数指针都要重新实现,并在新的操作表中赋值
+ * Copy or replace *file* operations
+ * All non-copy operations must be implemented and registered.
  */
 static int droidcry_file_open(struct inode * inode, struct file * filp)
 {
-	printk(KERN_ALERT "OOOOOOO: my own file open\n");
+	printk(KERN_ALERT "droidcry: my own file open\n");
 	return lower_fops->open(inode ,filp);
 } 
  
@@ -153,41 +140,68 @@ void droidcry_copy_file_operations(void)
 
 
 /**
- * 拷贝/替换　inode　的操作表内容
- * 凡是没有直接拷贝的函数指针都要重新实现,并在新的操作表中赋值
+ * Copy or replace *inode* operations
+ * All non-copy operations must be implemented and registered.
  */
-static struct dentry *droidcry_lookup(struct inode *dir, struct dentry *dentry, struct nameidata *nd) 
+static struct dentry *droidcry_lookup(struct inode *dir, struct dentry *dentry, 
+							unsigned int flags) 
 {
 	struct dentry *ret_dentry;
-	printk(KERN_ALERT "OOOOOOOO: My own lookup\n");
-	ret_dentry = lower_iops->lookup(dir,dentry,nd);
-	/* 这个dentry对应的inode可能不存在内存或磁盘上 */
-	if (!ret_dentry)
+	
+	printk(KERN_ALERT "droidcry: My own lookup\n");
+	ret_dentry = lower_iops->lookup(dir,dentry,flags);
+	if (!ret_dentry || !(ret_dentry->d_inode)) {
+		printk("droidcry: invalid dentry in lookup\n");
 		goto out;
-	printk("Modifying %s 's operations\n", ret_dentry->d_iname);
+	}
+	printk("droidcry: Modifying %s 's operations\n", ret_dentry->d_iname);
+	/* Save and replace fops */
+	if (ret_dentry->d_inode->i_fop == &droidcry_fops) 
+		return ret_dentry;
 	lower_fops = ret_dentry->d_inode->i_fop;	
 	droidcry_copy_file_operations();
 	ret_dentry->d_inode->i_fop = &droidcry_fops;
+	/* Save and replace aops */
+	if (ret_dentry->d_inode->i_mapping->a_ops == &droidcry_aops)
+		return ret_dentry;
+	lower_aops = ret_dentry->d_inode->i_mapping->a_ops;
+	droidcry_copy_address_space_operations();
+	ret_dentry->d_inode->i_mapping->a_ops = &droidcry_aops;
 out:
 	return ret_dentry;
-}
+} 
 
-static int droidcry_create(struct inode *dir, struct dentry *dentry, umode_t mode, struct nameidata *nd)
+static int droidcry_create(struct inode *dir, struct dentry *dentry, umode_t mode, 
+						bool b)
 {
 	int rc;
-	printk(KERN_ALERT "OOOOOOOO: My own create\n");
 	
-	rc = lower_iops->create(dir, dentry, mode, nd);
+	printk(KERN_ALERT "droidcry: My own create\n");
+	printk("droidcry: Modifying %s 's operations\n", dentry->d_iname);
+	rc = lower_iops->create(dir, dentry, mode, b);
+	/* Save and replace lower fops */
+	if (dentry->d_inode->i_fop == &droidcry_fops) 
+		return rc;
 	lower_fops = dentry->d_inode->i_fop; 
-	printk(KERN_ALERT "lower_fops (0x%x) \n", lower_fops);
 	if (lower_fops) {
-		printk(KERN_ALERT "OOOOOOOO: lower_fops is not null(0x%x) <- Is the value strange?\n", lower_fops);
+		printk("droidcry: Lower_fops is not null(0x%x)\n", lower_fops);
 		droidcry_copy_file_operations();
+		dentry->d_inode->i_fop = &droidcry_fops;
 	} else {
-		printk(KERN_ALERT "OOOOOOOO: lower_fops is null\n");
+		printk("droidcry: Lower_fops is null\n");
 	}
-	printk("Creation complete. Modifying %s 's operations\n", dentry->d_iname);
-	dentry->d_inode->i_fop = &droidcry_fops;
+	/* Save and replace lower fops */
+	if (dentry->d_inode->i_mapping->a_ops == &droidcry_aops)	
+		return rc;
+	lower_aops = dentry->d_inode->i_mapping->a_ops;
+	if (lower_aops) {
+		printk("droidcry: Lower_aops is not null(0x%x) <- Is the value strange?\n", 
+			lower_aops);
+		droidcry_copy_address_space_operations();
+		dentry->d_inode->i_mapping->a_ops = &droidcry_aops;
+	} else {
+		printk("droidcry: Lower_aops is null\n");
+	}
 	return rc;	
 }
 
@@ -196,9 +210,11 @@ struct inode_operations droidcry_iops = {
 	.create = droidcry_create,
 };
 
+/**
+ * make sure lower_iops is correctly pointed before invoke
+ */
 void droidcry_copy_inode_operations(void)
 {
-	/* 使用前确定lower_iops准备就绪 */
 	//droidcry_iops.create 	= lower_iops->create;
 	//droidcry_iops.lookup	= lower_iops->lookup;
 	droidcry_iops.link		= lower_iops->link;
@@ -213,22 +229,24 @@ void droidcry_copy_inode_operations(void)
 	droidcry_iops.fiemap    = lower_iops->fiemap;
 }
 
-
+static char *target;
+module_param(target,charp,0644);
  
 static int droidcry_init(void)
 {
-	const char * pathname = "/storage/sdcard";
+	const char * pathname = target;
 	struct path secret_path;
 		
-	printk(KERN_ALERT "OOOOOOOO: droidcry initializing ...\n");
+	printk("Droidcry initializing ...\n");
 	
 	kern_path(pathname, LOOKUP_FOLLOW, &secret_path);
-	/* 为加密目录保存之前的iops,然后替换之 */
+	/* Save iops of target directory and replace it */
 	lower_iops = secret_path.dentry->d_inode->i_op;
 	droidcry_copy_inode_operations();
-	/* TODO: 替换之前应该先检测目录或其中文件是否正在被使用 */
+	/* TODO: check if any file in the directory is in use */
+	
 	secret_path.dentry->d_inode->i_op = &droidcry_iops;
-	/* TODO: 只是一个平坦模型,如果需要支持目录,还得修改mkdir操作 */
+	/* TODO: modify the mkdir() to support subdirectory */
 	
 	return 0; 
 }
@@ -238,6 +256,7 @@ static int droidcry_init(void)
 static void droidcry_exit(void)
 {
 	printk(KERN_ALERT "Uninstalling droidcry ...\n");
+	//TODO: destruction
 }
 
 module_init(droidcry_init);
@@ -245,6 +264,4 @@ module_exit(droidcry_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Freeman Zhang");
-MODULE_DESCRIPTION("An simple transparent en/decrypt module for android");
-
-
+MODULE_DESCRIPTION("An simple transparent en/decrypt module for practice");
